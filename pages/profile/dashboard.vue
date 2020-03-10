a2d17587e3572dfcfb8f6390fe119607ce5cd0a1
armin <arminkheirkhahan@gmail.com>
2020-03-11 00:49:58 +0330

    add roadmap

--- pages/profile/dashboard.vue
+++ pages/profile/dashboard.vue
@@ -1,18 +1,27 @@
 <style lang="scss" scoped>
-img {
-  max-width: 100%;
-}
 </style>
 <template>
   <section>
-    <img src="/img/road_map_2.png" alt />
+    <!-- <img :src="img" alt /> -->
+    <Level :level="level+1" />
   </section>
 </template>
 <script lang="ts">
 import Vue from 'vue'
+import Level from '@/components/roadmap/roadmap.desktop.vue'
 export default Vue.extend({
+  components: {
+    Level
+  },
+  props: {
+    user: {
+      type: Object as () => any
+    }
+  },
   data() {
-    return {}
+    return {
+      level: 5
+    }
   }
 })
 </script>
